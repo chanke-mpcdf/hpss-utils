0ccb1e9d2d10ab36bce8eebe3aa68df8f4b3c701
Christof Hanke <christof.hanke@rzg.mpg.de>
2020-02-24 12:18:18 +0100

    hpss_purge: fix error-output
    It contained two commata in a row, breaking json-syntax.
    Also return 400 when a file could not be openened in HPSS,
    e.g. because it does not exist.

--- hpss-http_proxy/src/src/hpss_purge.c
+++ hpss-http_proxy/src/src/hpss_purge.c
@@ -36,6 +36,7 @@ do_hpss_purge(struct evbuffer *out_evb, char *escaped_path, int enclosed_json, u
                 evbuffer_add_printf(out_evb,
                                     "\"errstr\" : \"cannot open hpss-path %s.\", ",
                                     escaped_path);
+		rc = 400;
                 goto end;
         }
         rc = hpss_Purge(hfd, cast64m(0), cast64m(0), storage_level, BFS_PURGE_ALL,
@@ -45,11 +46,11 @@ do_hpss_purge(struct evbuffer *out_evb, char *escaped_path, int enclosed_json, u
 		escaped_path);
 
 	if (rc) {
-		evbuffer_add_printf(out_evb, "\"errstr\" : \"Cannot purge\", ");
-		evbuffer_add_printf(out_evb, "\"errno\" : \"%d\"", rc);
 		fprintf(serverInfo.LogFile,
 			"%s:%d:: hpss_Purge(\"%s\") returned rc=%d\n", __FILE__,
 			__LINE__, escaped_path, rc);
+		evbuffer_add_printf(out_evb, "\"errstr\" : \"Cannot purge\", ");
+		evbuffer_add_printf(out_evb, "\"errno\" : \"%d\", ", rc);
 		rc = 400;
                 goto end;
 	} else { 
@@ -60,10 +61,7 @@ do_hpss_purge(struct evbuffer *out_evb, char *escaped_path, int enclosed_json, u
  end:
 	if (enclosed_json) {
 		evbuffer_add_printf(out_evb, "}");
-	} else { 
-		evbuffer_add_printf(out_evb, ", ");
-        }
-
+	}
 
 	/*
 	 * Close the files, if required
